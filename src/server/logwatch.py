b1ab516c150507e3d97e9bf6fdd8d23e69f1a9c2
Nilesh Prasad <prasadnilesh96@gmail.com>
2020-02-09 00:06:09 +0530

    adding file path

--- src/server/logwatch.py
+++ src/server/logwatch.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
-
+import os
 import asyncio
 import random
 import websockets
+from pathlib import Path
 
 async def time(websocket, path):
-    f = open("logfile.txt", "r")
+    two_up = Path(__file__).resolve().parents[2]
+    print(two_up)
+    f = open(os.path.join(two_up,"logfile.txt"), "r")
     contents = f.read()
     print(contents)
     last_pos = f.tell()
     print(last_pos)
     await websocket.send(contents)
     while True:
-        f = open("logfile.txt", "r")
+        f = open(os.path.join(two_up,"logfile.txt"), "r")
         f.seek(last_pos)
         newText = f.readline()
         if newText!='':
             #   something new
             await websocket.send(newText)
             last_pos = f.tell()
-        # last_pos = f.tell()
         await asyncio.sleep(random.random() * 3)
 
 start_server = websockets.serve(time, "127.0.0.1", 5678)
