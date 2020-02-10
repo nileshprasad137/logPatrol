830413d6738fa9214112a7c92920bccd3bfdcaad
Nilesh Prasad <prasadnilesh96@gmail.com>
2020-02-11 01:03:29 +0530

    separating js file

---
+++ src/client/websocket.js
@@ -0,0 +1,18 @@
+var flag = false;
+var ws = new WebSocket("ws://127.0.0.1:5678/"),
+    messages = document.createElement('ul');
+ws.onmessage = function (event) {
+    var messages = document.getElementsByTagName('ul')[0],
+        message = document.createElement('li'),
+        content = document.createTextNode(event.data);
+    console.log(content)
+    if(content.data=="ping"){
+        console.log(content)
+        ws.send("pong")
+    }
+    else{
+        message.appendChild(content);
+        messages.appendChild(message);
+    }
+};
+document.body.appendChild(messages);
